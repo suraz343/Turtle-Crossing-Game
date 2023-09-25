c5978d5a43734377f8427b23e3d90b68a9251857
suraz343 <prajapati.suraj343@gmail.com>
2023-09-25 17:19:35 +0530

    Final Commit Project Completed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
 screen.tracer(0)
 
 player = Player()
@@ -22,5 +23,18 @@ while game_is_on:
     screen.update()
 
     car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
 
 screen.exitonclick()
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
@@ -9,10 +10,17 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def go_up(self):
-
         self.forward(MOVE_DISTANCE)
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
