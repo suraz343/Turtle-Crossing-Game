0b170daf01bac682b087904e592e5a0562375774
suraz343 <prajapati.suraj343@gmail.com>
2023-09-25 12:21:37 +0530

    some changes done on pplayer and car_manager file

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+car_manager = CarManager()
+
+scoreboard = Scoreboard()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+
+screen.exitonclick()
